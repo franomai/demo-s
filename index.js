f2e9d70f5302fbeb01bb4c8d320606b49ba6515e
ladiespls <fsta657@aucklanduni.ac.nz>
2018-05-20 11:13:12 +1200

    Add base tale, need to hook up the logic.
    Everybody makes mistakes

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 var Discord = require('discord.js');
+var fs = require('fs');
 var bot = new Discord.Client({autoReconnect: true});
+
+var stories = getStories('./stories');
+
+// Read everything into memory, move this out some other time.
+
+function getStories (dir) {
+  var storyFiles = fs.readdirSync(dir);
+  var stories = {};
+  for (var storyFileIndex = 0; storyFileIndex < storyFiles.length; storyFileIndex++) {
+    var story = storyFiles[storyFileIndex];
+    var filename = dir + '/' + story;
+    if (!fs.statSync(filename).isDirectory()) {
+      var contents = fs.readFileSync(filename, 'utf8');
+      stories[filename] = parseStory(contents);
+    }
+  }
+  return stories;
+}
+
+function parseStory (story) {
+  console.log(story);
+}
 
 bot.login(process.env.TOKEN);
 
